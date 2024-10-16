18c6b6c6abcf05a4fdc3e329504b0b14ace90b29
Mohammad Imran <immu.engineer@gmail.com>
2024-10-16 23:17:52 +0600

    added practice code

---
+++ practice2.js
@@ -0,0 +1,32 @@
+// const numbers1 = [1,2,3,4,5,6,7,8,9];
+// const numbers2 = [...numbers1];
+
+
+// numbers2.push(10);
+
+// console.log(numbers1);
+// console.log(numbers2);
+
+
+
+const student = {
+    name: 'Salib khan',
+    age: '32',
+    movies: ['no1','Dhaka attack','virtual']
+}
+
+
+// const studentJSON = JSON.stringify(student);
+
+// console.log(studentJSON);
+
+
+// const jsonparse = JSON.parse(studentJSON);
+
+// console.log(jsonparse);
+
+
+// const keys = Object.keys(student);
+// const value = Object.values(student);
+// console.log(keys);
+// console.log(value);
